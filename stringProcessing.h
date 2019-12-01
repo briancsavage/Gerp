a9c26a708aa332c4bf1144fa58ab1849924cb9da
briancsavage <47962267+briancsavage@users.noreply.github.com>
2019-12-01 11:45:00 -0500

    Create stringProcessing.h

---
+++ stringProcessing.h
@@ -0,0 +1,14 @@
+// Brian Savage
+// Gerp - Project 1: Part 2
+// Nov. 29th, 2019
+// stringProcessing.h
+// Purpose: Defintions for stringProcessing class
+
+#include <string>
+
+using namespace std;
+
+class stringProcessing {
+    public:
+        string stripNonAlphaNum(string original);
+};
